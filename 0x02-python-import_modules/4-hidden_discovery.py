1c035d9fb27dd4bddc5ac0e62aca9e2575b3ef65
kenialo19 <3396@holbertonschool.com>
2021-09-08 17:47:19 -0500

    prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     import hidden_4
-    sys.path.append('/mnt/c/Users/winda/kenialo/holbertonschool-higher_level_programming/0x02-python-import_modules')
+    sys.path.append('./')
     names = dir(hidden_4)
     for i in range(0, len(names)):
         if names[i][0:2] != "__":
