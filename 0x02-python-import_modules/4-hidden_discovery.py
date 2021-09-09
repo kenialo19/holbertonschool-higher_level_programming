8b046696ebacbd2dc4d78bd337460bba44acdbab
kenialo19 <3396@holbertonschool.com>
2021-09-08 20:09:18 -0500

    modified file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 import hidden_4
 if __name__ == "__main__":
     for i in dir(hidden_4):
-        if i[0] != "__":
-            print(i)
+        if i[0:2] != "__":
+            print("{}".format(i))
