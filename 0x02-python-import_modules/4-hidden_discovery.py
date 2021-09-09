df2af6d6a9330ec3ea24bb59711a69fba5d7524e
kenialo19 <3396@holbertonschool.com>
2021-09-08 20:21:53 -0500

    modified file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    for i in dir(hidden_4):
-        if i[0] != "_":
-            print("{}".format(i))
+    names = dir(hidden_4)
+    names_length = len(names)
+    for n in range(0, names_length):
+        if names[n][0] != "_":
+            print("{}".format(names[n]))
