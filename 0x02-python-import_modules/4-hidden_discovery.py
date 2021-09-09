fabbe7437a72514f6d78bcfc63094f86281ad277
kenialo19 <3396@holbertonschool.com>
2021-09-09 14:59:06 -0500

    fixing tabulation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import hidden_4, sys
+    import hidden_4
+    import sys
     sys.path.append('./')
     for i in dir(hidden_4):
         if i[0] != "_":
