3e908ce6c6246b9498a3dacce32ae6d7a0ec5770
kenialo19 <3396@holbertonschool.com>
2021-09-13 22:50:01 -0500

    adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = list(tuple_a)
+        tuple_a.append(0)
+        tuple_a.append(0)
+        tuple_a = tuple(tuple_a)
+    if len(tuple_b) < 2:
+        tuple_b = list(tuple_b)
+        tuple_b.append(0)
+        tuple_b.append(0)
+        tuple_b = tuple(tuple_b)
+    tupla_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tupla_c
