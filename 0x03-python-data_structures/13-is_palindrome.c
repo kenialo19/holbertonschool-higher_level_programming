38333572111643e0619e171e436d9a48cc9fd9c7
kenialo19 <3396@holbertonschool.com>
2021-09-14 18:37:10 -0500

    delete free

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,9 +12,6 @@ int is_palindrome(listint_t **head)
 
 	if (*head == NULL)
 	{
-		free_listint(re);
-		free_listint(aux);
-		free_listint(ptr);
 		return (1);
 	}
 	while (re != NULL)
@@ -33,14 +30,8 @@ int is_palindrome(listint_t **head)
 		}
 		else
 		{
-			free_listint(re);
-			free_listint(aux);
-			free_listint(ptr);
 			return (0);
 		}
 	}
-	free_listint(re);
-	free_listint(aux);
-	free_listint(ptr);
 	return (1);
 }
