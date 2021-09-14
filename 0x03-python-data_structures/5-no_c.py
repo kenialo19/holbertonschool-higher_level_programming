45d06ff53ef59befabe98b4b26fa2a596608ca30
kenialo19 <3396@holbertonschool.com>
2021-09-13 19:26:49 -0500

    delete line

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    characters =  'cC'
-    my_string = ''.join( x for x in my_string if x not in characters)
+    characters = 'cC'
+    my_string = ''.join(x for x in my_string if x not in characters)
     return my_string
