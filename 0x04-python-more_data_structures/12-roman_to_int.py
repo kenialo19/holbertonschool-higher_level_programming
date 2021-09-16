7b98c38ed080e848760b7386242f17a1a6acd8b3
kenialo19 <3396@holbertonschool.com>
2021-09-15 20:15:26 -0500

    converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     sum = 0
     dic_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     size_str = len(roman_string)
-    for i in range(size_str):
+    for i in range(0, size_str):
         if i != size_str - 1 and (dic_roman[roman_string[i + 1]] > dic_roman[roman_string[i]]):
             sum -= dic_roman[roman_string[i]]
         else:
