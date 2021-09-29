2fd42c40a529241e2700c06b5137b098b8d542c4
kenialo19 <3396@holbertonschool.com>
2021-09-28 23:22:24 -0500

    modified file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,14 +3,15 @@
 
 
 class Square:
-    def __init__(self, size=0, position=(0, 0)):
-        """inicilited size"""
-        self.__size = size
+    """Creates a Square class"""
 
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes Square"""
+        self.size = size
         try:
-            self.__position = position
-        except TypeError as error:
-            print(error)
+            self.position = position
+        except TypeError as err:
+            print(err)
 
     @property
     def position(self):
@@ -19,19 +20,15 @@ class Square:
     @position.setter
     def position(self, value):
         self.__position = value
-
         if type(value) is not tuple:
             self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
-        
-        elif len(value) is not 2:
+        elif len(value) != 2:
             self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
-
-        if type(value[0]) is not int or type(value[1]) is not int:
+        elif type(value[0]) is not int or type(value[1]) is not int:
             self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
-
         elif value[0] < 0 or value[1] < 0:
             self.__position = None
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -42,6 +39,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
