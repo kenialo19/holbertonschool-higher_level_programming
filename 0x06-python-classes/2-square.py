4adc615c7ea073d66dc09a338be0f20fe3e4a0fe
kenialo19 <3396@holbertonschool.com>
2021-09-28 20:14:02 -0500

    defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """init allow square class to be used"""
+    def __init__(self, size=0):
+        """asign private instance attribute size"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
