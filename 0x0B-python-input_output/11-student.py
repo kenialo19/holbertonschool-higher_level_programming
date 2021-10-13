6911eeff6e9e0518ebe4ed510d1cb7410ab3c57f
kenialo19 <3396@holbertonschool.com>
2021-10-12 23:26:20 -0500

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+        """Class"""
+        def __init__(self, first_name, last_name, age):
+                """Constructor"""
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
+
+        def to_json(self, wi=None):
+            """Method Returns: [type]: dict"""
+            if wi is None:
+                return self.__dict__
+            dict = {}
+            for i in wi:
+                if i in self.__dict__:
+                    dict[i] = self.__dict__[i]
+            return dict
+        
+        def reload_from_json(self, json):
+            """Replaces all attributes of the Student instance"""
+
+            for key, value in json.items():
+                self.__dict__[key] = value
+
