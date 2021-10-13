18c2be97e710e22de4e5c66b96531ee25ae54769
kenialo19 <3396@holbertonschool.com>
2021-10-12 23:42:30 -0500

    inserts a line of text to a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,9 @@
 def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file"""
     with open(filename, 'r', encoding='utf-8') as f:
-        pepe = f.readlines()
+        wi = f.readlines()
     with open(filename, "w", encoding="utf-8") as f:
-        for a in pepe:
+        for a in wi:
             f.write(a)
             if (search_string in a):
                 f.write(new_string)
