0d10c9ad5e283fcc374a9171fff8ea31addc9bd3
kenialo19 <3396@holbertonschool.com>
2021-10-12 23:23:43 -0500

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,12 +10,12 @@ class Student:
                 self.last_name = last_name
                 self.age = age
 
-        def to_json(self, attrs=None):
+        def to_json(self, wi=None):
             """Method Returns: [type]: dict"""
-            if attrs is None:
+            if wi is None:
                 return self.__dict__
             dict = {}
-            for i in attrs:
+            for i in wi:
                 if i in self.__dict__:
                     dict[i] = self.__dict__[i]
             return dict
