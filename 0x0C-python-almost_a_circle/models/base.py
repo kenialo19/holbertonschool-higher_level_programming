009789f29d08f95592c0528ea435bed5143b480d
kenialo19 <3396@holbertonschool.com>
2021-10-18 18:12:15 -0500

    creating functions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """This is class base"""
 
 
+import json
+
 class Base:
     """Class that manage id attribute in all future classes
     and to avoid duplicating the same code"""
@@ -16,3 +18,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dictionary to JSON string"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ JSON string to file"""
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            list_objs = [i.to_dictionary() for i in list_objs]
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list_objs))
+            
