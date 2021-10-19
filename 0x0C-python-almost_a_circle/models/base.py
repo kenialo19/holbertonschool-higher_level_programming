c92eaf7726be12bfb3ab94d33f1d11a2c44dab19
kenialo19 <3396@holbertonschool.com>
2021-10-18 20:31:14 -0500

    creating functions and delete lines

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,25 @@ class Base:
             list_objs = [i.to_dictionary() for i in list_objs]
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+
+        if cls.__name__ == "Square":
+            dummy = cls(9)
+            dummy.update(**dictionary)
+            return dummy
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(9, 5)
+            dummy.update(**dictionary)
+            return dummy
