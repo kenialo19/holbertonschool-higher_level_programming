200015fced0f5674892ec9f024181dc028b97fd6
kenialo19 <3396@holbertonschool.com>
2021-11-17 19:12:06 -0500

    creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+-- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+-- The database name will be passed as an argument of the mysql command
+SELECT id, name FROM cities WHERE state_id = (SELECT id from states where name = 'California') ORDER BY id ASC;
