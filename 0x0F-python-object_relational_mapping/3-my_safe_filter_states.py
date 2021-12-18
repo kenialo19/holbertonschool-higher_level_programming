de9d92d18cacdea0001d813043d74232299c2c7b
kenialo19 <3396@holbertonschool.com>
2021-12-18 16:33:43 -0500

    write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument."""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1], password=argv[2], database=argv[3], port=3306)
+    cursor = db.cursor()
+    argument = argv[4]
+    cursor.execute(
+        'SELECT * FROM states WHERE name= %(argument)s ORDER BY states.id ASC', {'argument': argument})
+
+    state = cursor.fetchall()
+
+    for state in state:
+        print(state)
