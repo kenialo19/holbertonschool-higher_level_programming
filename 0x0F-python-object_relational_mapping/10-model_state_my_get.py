65ec0921ff8e6b472196bf165e53cf0ce0c2ceb9
kenialo19 <3396@holbertonschool.com>
2021-12-18 16:31:29 -0500

    modified file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,7 +5,7 @@ name passed as argument from the database hbtn_0e_6_usa"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
@@ -14,14 +14,15 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    id_position = argv[4]
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    id_position = argv[4]
-    db = session.query(State).filter(State.name == id_position).first()
+    db = session.query(State).order_by(State.id).filter(State.name == id_position).first()
     if db is None:
         print('Not found')
     else:
         print("{}".format(db.id))
+
     session.close()
