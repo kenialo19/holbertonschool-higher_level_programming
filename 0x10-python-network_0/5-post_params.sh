64f792d1dc5d10b6ffe0c65529ad78e7230aa1e1
kenialo19 <3396@holbertonschool.com>
2022-01-10 19:51:57 -0500

    modified file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request
-curl -s -d  "email=hr@test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -d  "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
