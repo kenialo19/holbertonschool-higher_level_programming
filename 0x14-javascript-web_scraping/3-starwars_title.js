9b337cd91af9c4403d3acff194a0889e07749bca
kenialo19 <3396@holbertonschool.com>
2022-02-02 18:23:10 -0500

    modified file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+// script that prints the title of a Star Wars movie where
+// the episode number matches a given integer
+
 const request = require('request');
 const url = `https://swapi.co/api/films/${process.argv[2]}/`;
 
